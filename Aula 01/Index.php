46ee632508a2f6ed9bb4ce968cb1b63d891eff3f
Diego Miguel <50773515+dhmiguel@users.noreply.github.com>
2019-09-03 15:34:53 -0300

    Create Index.php

---
+++ Aula 01/Index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     <title>Atividade Maior</title>
+     <meta charset="utf-8" />
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+
+  <body>
+    <div class="container border m-4">
+         
+        <h1>Verificar o Maior Número</h1>
+        
+        <form action="Maior.php" method="post">
+            <p>
+                Valor 1: <input type="number" name="val1" required/>
+            
+            </p>
+            
+            <p>
+                Valor 2: <input type="number" name="val2" required/>
+            
+            </p>
+            <p>
+                <input type="submit" value="Verificar"/>
+            
+            </p>
+                
+        
+        </form>
+        
+    </div>
+  </body>
+</html>
